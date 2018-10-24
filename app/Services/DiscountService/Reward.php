4c201cf0113b4c7cb0c137de8056898b1aa47cfe
Ashley <ashley@digitag.co>
2018-10-24 17:47:34 +0200

    rewards refinement

--- app/Services/DiscountService/Reward.php
+++ app/Services/DiscountService/Reward.php
@@ -20,73 +20,76 @@ class Reward {
     // discounts *limit not yet integrated with discounts, would only apply to the productline though*
     public function discount(Order $order, Array $data) {
         if ($number = $this->discount->getRewardNumber()) {
+
             // discounts can be applied anywhere. we'll default to the order
             $applyRewardTo = $this->discount->getApplyRewardTo() ?? 'order';
 
+            // not used with discounts yet, but ready to integrate
             $limit = $this->discount->getLimit();
-            $minus = 0;
 
             if ($applyRewardTo == 'order') {
-                $minus = $this->getDiscount($order->total, $number);
-                $order->total -= $minus;
+                $this->processOrder($order, $number);
             }
 
             elseif ($applyRewardTo == 'productLine') {
-                foreach ($order->items as &$item) {
-                    if (in_array($item['product-id'], $data)) {
-                        $minus = $this->getDiscount($item['total'], $number);
-                        $item['total'] -= $minus;
-                        $item['total'] = number_format($item['total'], 2);
-                        $order->total = $order->total - $minus;
-                    }
-                }
+                $this->processProductLine($order, $data, $number);
             }
 
             elseif ($applyRewardTo == 'cheapestItem') {
-                $items = $order->items;
-
-                usort($items, function($a, $b) {
-                    if ($a['unit-price'] == $b['unit-price']) return 0;
-                    return ($a['unit-price'] < $b['unit-price']) ? -1 : 1;
-                });
-
-                foreach ($items as &$item) {
-                    if (in_array($item['product-id'], $data)) {
-                        $minus = $this->getDiscount($item['unit-price'], $number);
-                        $item['unit-price'] -= $minus;
-                        $item['unit-price'] = number_format($item['unit-price'], 2);
-                        $order->total = $order->total - $minus;
-                        break;
-                    }
-                }
+                $this->processCheapestItem($order, $data, $number, 'unit-price');
             }
 
             elseif ($applyRewardTo == 'cheapestProduct') {
-                $items = &$order->items;
-
-                usort($items, function($a, $b) {
-                    if ($a['unit-price'] == $b['unit-price']) return 0;
-                    return ($a['unit-price'] < $b['unit-price']) ? -1 : 1;
-                });
-
-                foreach ($items as &$item) {
-                    if (in_array($item['product-id'], $data)) {
-                        $minus = $this->getDiscount($item['total'], $number);
-                        $item['total'] -= $minus;
-                        $item['total'] = number_format($item['total'], 2);
-                        $order->total = $order->total - $minus;
-                        break;
-                    }
-                }
-
+                $this->processCheapestItem($order, $data, $number, 'total');
             }
 
+            // reformat the number to a string, so that it matches the rest of the data
             $order->total = number_format($order->total, 2);
 
+            // add to discount array
             $order->discounts[] = $this->discount->getDescription();
         }
 
         $order->total = strval($order->total);
+    }
+
+    // apply discount to order
+    public function processOrder(Order $order, $reward) {
+        $minus = $this->getDiscount($order->total, $reward);
+        $order->total -= $minus;
+    }
+
+    // apply discount to product line
+    public function processProductLine(Order $order, $reward) {
+        foreach ($order->items as &$item) {
+            if (in_array($item['product-id'], $data)) {
+                $minus = $this->getDiscount($item['total'], $reward);
+                $item['total'] -= $minus;
+                $item['total'] = number_format($item['total'], 2);
+                $order->total = $order->total - $minus;
+            }
+        }
+    }
+
+    // apply discount to cheapest item or
+    // apply discount to productline of cheapest item
+    public function processCheapestItem(Order $order, Array $data, $reward, $property) {
+        $items = &$order->items;
+        
+        usort($items, function($a, $b) {
+            if ($a['unit-price'] == $b['unit-price']) return 0;
+            return ($a['unit-price'] < $b['unit-price']) ? -1 : 1;
+        });
+
+        foreach ($items as &$item) {
+            if (in_array($item['product-id'], $data)) {
+                $minus = $this->getDiscount($item[$property], $reward);
+                $item[$property] -= $minus;
+                $item[$property] = number_format($item[$property], 2);
+                $order->total = $order->total - $minus;
+                break;
+            }
+        }
     }
 
     // extra items
